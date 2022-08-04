fea1f35243fa5d1c9b70f66e374a73e2f173d20a
Hilmi <nurhilmi.mail@gmail.com>
2022-08-04 21:16:38 +0700

    remove file variable

--- src/variable/Url.jsx
+++
@@ -1,6 +0,0 @@
-const baseUrl = `http://localhost:8000`; // development
-// const baseUrl = `http://api-asset.terakom.id`; // production
-const apiUrl = `${baseUrl}/api`;
-const title = ` - Asset`;
-
-export { baseUrl, apiUrl, title };
