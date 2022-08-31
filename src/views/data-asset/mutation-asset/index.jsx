1ab98b3fc532f46e944dfb747e50daf7aa22dcea
Hilmi <nurhilmi.mail@gmail.com>
2022-08-31 22:30:30 +0700

    add filter mutation, maintenance & acceptance

--- src/views/data-asset/mutation-asset/index.jsx
+++ src/views/data-asset/mutation-asset/index.jsx
@@ -18,9 +18,22 @@ import {
    Menu,
    MenuItem,
    ListItemIcon,
+   ListItemText,
+   Select,
+   OutlinedInput,
+   Dialog,
+   DialogTitle,
+   DialogContent,
+   DialogActions,
+   Box,
+   Chip,
+   FormControl,
+   InputLabel,
+   Checkbox,
 } from "@mui/material";
 import { AddRounded, CloseRounded, Delete, Download, Edit, FilterListRounded, MoreVert, Search } from "@mui/icons-material";
 import { Link as RouterLink } from "react-router-dom";
+import { LoadingButton } from "@mui/lab";
 
 import http from "../../../component/api/Api";
 import Loading from "../../../component/Loading";
@@ -33,14 +46,27 @@ import { Permission } from "../../../component/Permission";
 export default function MutationAsset() {
    const { user } = useRecoilValue(authentication);
 
+   const [data, setData] = useState();
+   const [rows, setRows] = useState();
    const [params, setParams] = useState({
       search: "",
       paginate: 1,
       limit: 10,
       page: 1,
+      pic_id: [],
+      receive_id: [],
+      from_branch_id: [],
+      to_branch_id: [],
    });
 
-   const [data, setData] = useState();
+   const getUsers = async () => {
+      const res = await http.get(`user?paginate=0`);
+      return res.data.data.data;
+   };
+   const getLocation = async () => {
+      const res = await http.get(`location`);
+      return res.data.data;
+   };
    const getData = async () => {
       await http
          .get(`asset_mutation`, {
@@ -49,6 +75,8 @@ export default function MutationAsset() {
          .then((res) => {
             // console.log(res.data.data);
             setData(res.data.data);
+            setLoading(false);
+            setDialog(false);
          })
          .catch((err) => {
             // console.log(err.response);
@@ -64,6 +92,50 @@ export default function MutationAsset() {
       return () => clearTimeout(timer);
       // eslint-disable-next-line react-hooks/exhaustive-deps
    }, [params]);
+
+   const getAll = async (e) => {
+      let mounted = true;
+      if (mounted) {
+         Promise.all([getUsers(), getLocation()]).then((res) => {
+            handleComplete();
+            setRows({
+               ...rows,
+               users: res[0],
+               location: res[1],
+            });
+         });
+      }
+   };
+
+   const handleSubmit = async (e) => {
+      e.preventDefault();
+      handleLoading();
+      setParams({
+         ...params,
+         pic_id: filter.pic_id,
+         receive_id: filter.receive_id,
+         from_branch_id: filter.from_branch_id,
+         to_branch_id: filter.to_branch_id,
+      });
+   };
+
+   const handleReset = async (e) => {
+      e.preventDefault();
+      handleDialog();
+      setParams({
+         ...params,
+         pic_id: [],
+         receive_id: [],
+         from_branch_id: [],
+         to_branch_id: [],
+      });
+      setFilter({
+         pic_id: [],
+         receive_id: [],
+         from_branch_id: [],
+         to_branch_id: [],
+      });
+   };
 
    const handleSearch = (e) => {
       setParams({
@@ -117,6 +189,35 @@ export default function MutationAsset() {
       setAnchorEl(null);
    };
 
+   const [complete, setComplete] = useState(false);
+   const handleComplete = () => {
+      setComplete(!complete);
+   };
+   const [dialog, setDialog] = useState(false);
+   const handleDialog = () => {
+      complete === false && getAll();
+      setDialog(!dialog);
+   };
+   const [loading, setLoading] = useState(false);
+   const handleLoading = () => {
+      setLoading(!loading);
+   };
+   const [filter, setFilter] = useState({
+      pic_id: [],
+      receive_id: [],
+      from_branch_id: [],
+      to_branch_id: [],
+   });
+   const handleChange = async (e) => {
+      const {
+         target: { value },
+      } = e;
+      setFilter({
+         ...filter,
+         [e.target.name]: typeof value === "string" ? value.split(",") : value,
+      });
+   };
+
    return (
       <div className="main-content mb-5">
          <div className="page-content">
@@ -159,11 +260,11 @@ export default function MutationAsset() {
                               fullWidth
                            />
                         </Grid>
-                        {/* <Grid item>
-                           <Button variant="link" startIcon={<FilterListRounded />}>
+                        <Grid item>
+                           <Button variant="link" startIcon={<FilterListRounded />} onClick={handleDialog}>
                               Filter
                            </Button>
-                        </Grid> */}
+                        </Grid>
                      </Grid>
                      <TableContainer>
                         <Table sx={{ minWidth: 650, mt: 2 }} aria-label="simple table">
@@ -270,14 +371,14 @@ export default function MutationAsset() {
                            Edit
                         </MenuItem>
                      )}
-                     {staging !== undefined && staging.document !== null && 
-                     <MenuItem component="a" href={staging.document} target="_blank" >
-                        <ListItemIcon>
-                           <Download />
-                        </ListItemIcon>
-                        Download Support Document
-                     </MenuItem>
-                     }
+                     {staging !== undefined && staging.document !== null && (
+                        <MenuItem component="a" href={staging.document} target="_blank">
+                           <ListItemIcon>
+                              <Download />
+                           </ListItemIcon>
+                           Download Support Document
+                        </MenuItem>
+                     )}
                      {/* {Permission(user.permission, "delete asset mutation") && (
                         <MenuItem onClick={handleModal}>
                            <ListItemIcon>
@@ -288,6 +389,145 @@ export default function MutationAsset() {
                      )} */}
                   </Menu>
                ) : null}
+               <Dialog open={dialog} onClose={handleDialog} maxWidth="md" fullWidth>
+                  <DialogTitle>Filter</DialogTitle>
+                  {complete ? (
+                     <DialogContent dividers>
+                        <Grid container spacing={2}>
+                           <Grid item xs={12} md={6}>
+                              <FormControl sx={{ mt: 1 }} fullWidth>
+                                 <InputLabel>PIC Asset</InputLabel>
+                                 <Select
+                                    multiple
+                                    name="pic_id"
+                                    value={filter.pic_id}
+                                    onChange={handleChange}
+                                    input={<OutlinedInput label="PIC Asset" />}
+                                    renderValue={(selected) => {
+                                       return rows.users
+                                          .filter((v) => selected.includes(v.id))
+                                          .map((v, i) => {
+                                             return <Chip key={i} label={`${v.code} - ${v.name}`} onDelete={() => "cemas"} sx={{ mr: 0.5 }} />;
+                                          });
+                                    }}
+                                 >
+                                    {rows.users.length > 0 &&
+                                       rows.users.map((v, i) => {
+                                          return (
+                                             <MenuItem key={i} value={v.id}>
+                                                <Checkbox checked={filter.pic_id.indexOf(v.id) > -1} />
+                                                <ListItemText primary={`${v.code} - ${v.name}`} />
+                                             </MenuItem>
+                                          );
+                                       })}
+                                 </Select>
+                              </FormControl>
+                           </Grid>
+                           <Grid item xs={12} md={6}>
+                              <FormControl sx={{ mt: 1 }} fullWidth>
+                                 <InputLabel>Receive Name</InputLabel>
+                                 <Select
+                                    multiple
+                                    name="receive_id"
+                                    value={filter.receive_id}
+                                    onChange={handleChange}
+                                    input={<OutlinedInput label="Receive Name" />}
+                                    renderValue={(selected) => {
+                                       return rows.users
+                                          .filter((v) => selected.includes(v.id))
+                                          .map((v, i) => {
+                                             return <Chip key={i} label={`${v.code} - ${v.name}`} onDelete={() => "cemas"} sx={{ mr: 0.5 }} />;
+                                          });
+                                    }}
+                                 >
+                                    {rows.users.length > 0 &&
+                                       rows.users.map((v, i) => {
+                                          return (
+                                             <MenuItem key={i} value={v.id}>
+                                                <Checkbox checked={filter.receive_id.indexOf(v.id) > -1} />
+                                                <ListItemText primary={`${v.code} - ${v.name}`} />
+                                             </MenuItem>
+                                          );
+                                       })}
+                                 </Select>
+                              </FormControl>
+                           </Grid>
+                           <Grid item xs={12} md={6}>
+                              <FormControl sx={{ mt: 1 }} fullWidth>
+                                 <InputLabel>From Branch</InputLabel>
+                                 <Select
+                                    multiple
+                                    name="from_branch_id"
+                                    value={filter.from_branch_id}
+                                    onChange={handleChange}
+                                    input={<OutlinedInput label="From Branch" />}
+                                    renderValue={(selected) => {
+                                       return rows.location
+                                          .filter((v) => selected.includes(v.id))
+                                          .map((v, i) => {
+                                             return <Chip key={i} label={`${v.code} - ${v.location}`} onDelete={() => "cemas"} sx={{ mr: 0.5 }} />;
+                                          });
+                                    }}
+                                 >
+                                    {rows.location.length > 0 &&
+                                       rows.location.map((v, i) => {
+                                          return (
+                                             <MenuItem key={i} value={v.id}>
+                                                <Checkbox checked={filter.from_branch_id.indexOf(v.id) > -1} />
+                                                <ListItemText primary={`${v.code} - ${v.location}`} />
+                                             </MenuItem>
+                                          );
+                                       })}
+                                 </Select>
+                              </FormControl>
+                           </Grid>
+                           <Grid item xs={12} md={6}>
+                              <FormControl sx={{ mt: 1 }} fullWidth>
+                                 <InputLabel>To Branch</InputLabel>
+                                 <Select
+                                    multiple
+                                    name="to_branch_id"
+                                    value={filter.to_branch_id}
+                                    onChange={handleChange}
+                                    input={<OutlinedInput label="To Branch" />}
+                                    renderValue={(selected) => {
+                                       return rows.location
+                                          .filter((v) => selected.includes(v.id))
+                                          .map((v, i) => {
+                                             return <Chip key={i} label={`${v.code} - ${v.location}`} onDelete={() => "cemas"} sx={{ mr: 0.5 }} />;
+                                          });
+                                    }}
+                                 >
+                                    {rows.location.length > 0 &&
+                                       rows.location.map((v, i) => {
+                                          return (
+                                             <MenuItem key={i} value={v.id}>
+                                                <Checkbox checked={filter.to_branch_id.indexOf(v.id) > -1} />
+                                                <ListItemText primary={`${v.code} - ${v.location}`} />
+                                             </MenuItem>
+                                          );
+                                       })}
+                                 </Select>
+                              </FormControl>
+                           </Grid>
+                        </Grid>
+                     </DialogContent>
+                  ) : (
+                     <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: "60vh" }}>
+                        <Loading />
+                     </Box>
+                  )}
+                  {complete && (
+                     <DialogActions sx={{ py: 2, px: 3 }}>
+                        <LoadingButton variant="outlined" onClick={handleReset}>
+                           Reset
+                        </LoadingButton>
+                        <LoadingButton loading={loading} variant="contained" onClick={handleSubmit}>
+                           Apply
+                        </LoadingButton>
+                     </DialogActions>
+                  )}
+               </Dialog>
             </div>
          </div>
       </div>
